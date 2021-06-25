53ea6ce37843bb47ab3522aba1f56165834af066
Mark Melton <mark.melton@whitemice.io>
2021-06-25 21:27:22 +0000

    Added comments to bzip Compressor.

--- include/core/codex/bzip/compressor.h
+++ include/core/codex/bzip/compressor.h
@@ -7,15 +7,25 @@
 
 namespace bzip {
 
+// Write bytes compressed using the bzip2 library to `Sink`.
+//
 template<class Sink>
 class Compressor {
 public:
+    // Construct a compressor that will write to `sink` using a buffer
+    // of size `n`.
     Compressor(Sink& sink, size_t n = 65536);
+
+    // Flush any remaining data and free all resources.
     ~Compressor();
 
+    // Flush any remaining data and free all resources.
     void close();
-    void write(const char *input, size_t input_len);
-    
+
+    // Write the data from `begin` to `being` + `count` to the `Sink`.
+    void write(const char *begin, size_t count);
+
+    // Write the raw bytes representing the pod-type `value` to the `Sink`.
     template<class T>
     void write_pod(T& value) { write(reinterpret_cast<const char*>(&value), sizeof(T)); }
 
