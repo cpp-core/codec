d0f232c97086a08b3d9a2d07b4808dd6034fcd9f
Mark Melton <mark.melton@notlem.com>
2021-07-31 22:51:30 -0400

    Added filter_comments helper and fixed filter_stream bugs.

--- test/src/core/codex/test_filter.cpp
+++ test/src/core/codex/test_filter.cpp
@@ -5,12 +5,13 @@
 #include <range/v3/view/take.hpp>
 #include <sstream>
 #include "core/codex/filter.h"
+#include "core/codex/filter_comments.h"
 #include "core/range/sample.h"
 #include "core/range/string.h"
 
 static const int NumberSamples = 64;
 
-TEST(Stream, Filter)
+TEST(Codex, Filter)
 {
     const int MaxLineSize = 128;
     const int LineFilter = 64;
@@ -21,24 +22,98 @@ TEST(Stream, Filter)
 	auto generator = cr::str::alpha(gsize);
 
 	uint expected_count{0};
-	for (auto str : generator | v::take(64)) {
-	    if (str.size() < LineFilter)
+	for (auto str : generator | v::take(MaxLineSize)) {
+	    str += '\n';
+	    if (str.size() < LineFilter) {
 		++expected_count;
+	    }
 	    lines += str;
-	    lines += '\n';
 	}
+	
 	std::stringstream ss{lines};
 	core::filter_istream fin(ss, [](string_view s) { return s.size() < LineFilter; });
+	
 	string line;
 	uint count{0};
 	while (std::getline(fin, line)) {
-	    if (line.size() < LineFilter)
-		++count;
+	    ++count;
 	    EXPECT_LT(line.size(), LineFilter);
 	}
 	EXPECT_EQ(count, expected_count);
     }
 }
+
+size_t count_lines(const string& str) {
+    std::stringstream ss{str};
+    auto sin = core::filter_comments(ss);
+    
+    string line;
+    size_t count{0};
+    while (std::getline(sin, line))
+	++count;
+    return count;
+}
+
+TEST(CodexFilter, CommentsNoNewline)
+{
+    auto str = R"(// Artifact properties test0
+//
+{
+    "tsdb::Instrument" : "BTC/USD",
+    "tsdb::Exchange" : "kraken",
+    "tsdb::Organization" : "per-instrument",
+    "tsdb::Source" : "next-prime"
+})";
+
+    EXPECT_EQ(count_lines(str), 6);
+}
+
+TEST(CodexFilter, CommentsNewline)
+{
+    auto str = R"(// Artifact properties test0
+//
+{
+    "tsdb::Instrument" : "BTC/USD",
+    "tsdb::Exchange" : "kraken",
+    "tsdb::Organization" : "per-instrument",
+    "tsdb::Source" : "next-prime"
+}
+)";
+
+    EXPECT_EQ(count_lines(str), 6);
+}
+
+TEST(CodexFilter, CommentsCommentNoNewline)
+{
+    auto str = R"(// Artifact properties test0
+//
+{
+    "tsdb::Instrument" : "BTC/USD",
+    "tsdb::Exchange" : "kraken",
+    "tsdb::Organization" : "per-instrument",
+    "tsdb::Source" : "next-prime"
+}
+// end)";
+
+    EXPECT_EQ(count_lines(str), 6);
+}
+
+TEST(CodexFilter, CommentsCommentNewline)
+{
+    auto str = R"(// Artifact properties test0
+//
+{
+    "tsdb::Instrument" : "BTC/USD",
+    "tsdb::Exchange" : "kraken",
+    "tsdb::Organization" : "per-instrument",
+    "tsdb::Source" : "next-prime"
+}
+// end
+)";
+
+    EXPECT_EQ(count_lines(str), 6);
+}
+
 
 int main(int argc, char *argv[])
 {
